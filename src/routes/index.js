2b710a8c27d9abd48ad306a315641db080258de6
haqsir@163.com <haqsir@163.com>
2022-05-31 21:18:11 +0800

    excel文件上传 mysql

---
+++ src/routes/index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const router = express.Router();
+const excelController = require("../controllers/ExcelCcontroller");
+const upload = require("../middlewares/upload");
+
+let routes = (app) => {
+  router.post("/upload", upload.single("file"), excelController.upload);
+  router.get("/list", excelController.getTutorials);
+
+  router.get("/download", excelController.download);
+
+  return app.use("/api/excel", router);
+};
+
+module.exports = routes;
