622125023faf6654555f71ccaf84d5a594b7a10b
haqsir@163.com <haqsir@163.com>
2022-05-31 23:14:16 +0800

    excel 文件上传

--- src/middlewares/upload.js
+++ src/middlewares/upload.js
@@ -17,7 +17,7 @@ var storage = multer.diskStorage({
   },
   filename: (req, file, cb) => {
     console.log(file.originalname);
-    cb(null, `${Date.now()}-${file.originalname}`);
+    cb(null, `${Date.now()}-zhijianqiu-${file.originalname}`);
   },
 });
 
